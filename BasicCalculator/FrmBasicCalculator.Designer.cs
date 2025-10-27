118407ca76e8bfc3a0e1f4b0fe255ea4808fee6b
janelfrancisco <janelfrancisco2122@gmail.com>
2025-10-27 19:41:33 +0800

    imported the class library, added the methods, and updated the UI

--- BasicCalculator/FrmBasicCalculator.Designer.cs
+++ BasicCalculator/FrmBasicCalculator.Designer.cs
@@ -32,8 +32,11 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.cbxOperation = new System.Windows.Forms.ComboBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.btnCompute = new Guna.UI2.WinForms.Guna2Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -53,6 +56,7 @@
             this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Desktop;
             this.textBox1.Location = new System.Drawing.Point(64, 97);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(270, 33);
@@ -63,6 +67,7 @@
             this.cbxOperation.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.cbxOperation.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cbxOperation.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxOperation.ForeColor = System.Drawing.SystemColors.MenuText;
             this.cbxOperation.FormattingEnabled = true;
             this.cbxOperation.Items.AddRange(new object[] {
             "+",
@@ -79,37 +84,64 @@
             this.textBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.MenuText;
             this.textBox2.Location = new System.Drawing.Point(64, 207);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(270, 33);
             this.textBox2.TabIndex = 3;
             // 
-            // richTextBox1
+            // btnCompute
             // 
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(64, 271);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(270, 86);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
+            this.btnCompute.BackColor = System.Drawing.Color.Transparent;
+            this.btnCompute.BorderRadius = 6;
+            this.btnCompute.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCompute.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCompute.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCompute.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCompute.FillColor = System.Drawing.Color.Gray;
+            this.btnCompute.Font = new System.Drawing.Font("Century Gothic", 10.25F, System.Drawing.FontStyle.Bold);
+            this.btnCompute.ForeColor = System.Drawing.Color.White;
+            this.btnCompute.Location = new System.Drawing.Point(142, 379);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(120, 33);
+            this.btnCompute.TabIndex = 6;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
             // 
-            // guna2Button1
+            // label2
             // 
-            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2Button1.BorderRadius = 6;
-            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button1.FillColor = System.Drawing.Color.Gray;
-            this.guna2Button1.Font = new System.Drawing.Font("Century Gothic", 10.25F, System.Drawing.FontStyle.Bold);
-            this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Location = new System.Drawing.Point(142, 379);
-            this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(120, 33);
-            this.guna2Button1.TabIndex = 6;
-            this.guna2Button1.Text = "Compute";
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(76, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Total:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 271);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 82);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(136, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "00";
             // 
             // FrmBasicCalculator
             // 
@@ -118,8 +150,10 @@
             this.BackgroundImage = global::BasicCalculator.Properties.Resources.e6d6d0663a89645d6427289b61f86e90;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(396, 437);
-            this.Controls.Add(this.guna2Button1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCompute);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.cbxOperation);
             this.Controls.Add(this.textBox1);
@@ -129,6 +163,7 @@
             this.Name = "FrmBasicCalculator";
             this.Text = "FrmBasicCalculator";
             this.Load += new System.EventHandler(this.FrmBasicCalculator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,8 +175,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox cbxOperation;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button btnCompute;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- BasicCalculator/FrmBasicCalculator.cs
+++ BasicCalculator/FrmBasicCalculator.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CalculatorPrivateAssembly;
 
 namespace BasicCalculator
 {
@@ -18,6 +19,51 @@ namespace BasicCalculator
         }
 
         private void FrmBasicCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            float num1 = float.Parse(textBox1.Text);
+            float num2 = float.Parse(textBox2.Text);    
+            string operation = cbxOperation.SelectedItem.ToString();
+            float result = 0;
+
+            if (operation == "+")
+            {
+                result = BasicComputation.Add((int)num1, (int)num2);
+            }
+            else if (operation == "-")
+            {
+                result = BasicComputation.Subtract((int)num1, (int)num2);
+            }
+            else if (operation == "x")
+            {
+                result = BasicComputation.Multiply((int)num1, (int)num2);
+            }
+            else if (operation == "/")
+            {
+                try
+                {
+                    result = BasicComputation.Divide((int)num1, (int)num2);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            label3.Text = result.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
